ae03bcaf896798d241ad7187fb17773e342f8550
Giovanne Leite <166829695+giovanne7@users.noreply.github.com>
2025-05-05 20:41:44 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
-require("dotenv").config();
-const app = require('../app');
+import dotenv from 'dotenv';
+import app from '../app'; // ajuste o caminho conforme necessário
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Servidor ativo na porta ${PORT}`);
-  console.log(
-    `Documentação Swagger disponível em http://localhost:${PORT}/api-docs`
-  );
+  console.log(`✅ Servidor ativo na porta ${PORT}`);
+  console.log(`📄 Documentação Swagger: http://localhost:${PORT}/api-docs`);
 });
