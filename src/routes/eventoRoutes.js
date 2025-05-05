118048f878d1d8b220a64efc9dff9abc3d2d7494
Giovanne Leite <166829695+giovanne7@users.noreply.github.com>
2025-05-05 20:15:42 -0300

    Update eventoRoutes.js

--- src/routes/eventoRoutes.js
+++ src/routes/eventoRoutes.js
@@ -5,6 +5,14 @@ const authMiddleware = require("../middlewares/authMiddleware");
 const verificaTipoUsuario = require("../middlewares/verificaTipoUsuario");
 const supabase = require("../config/supabaseClient");
 const { sendSuccess, sendError } = require("../utils/responseFormatter");
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+  origin: 'https://tixupfrontend.vercel.app',
+  credentials: true,
+}));
 
 // Rota pública para listar eventos
 router.get(
